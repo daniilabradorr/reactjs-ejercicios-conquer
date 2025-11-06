21d819506b90827d0346afbb89b8e1790cc1edc9
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-11-06 13:47:45 +0100

    fix: arreglando  el jercicio 3 y añadiendo el evento de la tecla enter al ej3 y 9. solo faltaria hacer correctamente el readme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// (opcional) si tienes estilos globales en src/index.css
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
